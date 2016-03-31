495d9b6dacf4b9275ef1d30d318e7035aed18b19
Hector Romero <hectorromero@Hectors-MacBook-Pro.local>
2016-03-31 16:14:26 -0700

    add gzippo

--- dist/scripts/config.9eba1fa1.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
- angular.module('config', [])
-
-.constant('ENV', {name:'production',apiEndpoint:'https://fast-brook-40517.herokuapp.com'})
-
-;
---
+++ dist/scripts/config.ee2d85f6.js
@@ -0,0 +1,7 @@
+"use strict";
+
+ angular.module('config', [])
+
+.constant('ENV', {name:'production',apiEndpoint:'https://cryptic-fortress-59506.herokuapp.com/'})
+
+;
